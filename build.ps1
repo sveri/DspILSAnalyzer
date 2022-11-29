404909d6f75b38b39fa7e7d2dd703e0040651ad8
Sven Richter <sveri80@gmail.com>
2022-11-29 20:33:28 +0100

    release 0.1.0

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 dotnet build --configuration Release
 
-mkdir temp
+New-Item temp -Type Directory -Force
 
 cp .\bin\Release\net472\DspILSAnalyzer.dll temp
 cp .\bin\Release\net472\DspILSAnalyzer.pdb temp
@@ -8,5 +8,14 @@ cp .\icon.png temp
 cp .\manifest.json temp
 cp .\README.md temp
 
+
+Remove-Item .\DspILSAnalyzer.zip -Force
+cd temp
+
+Compress-Archive -Path .\*.* DspILSAnalyzer.zip
+
+mv .\DspILSAnalyzer.zip ..\
+
+cd ..
 
 Remove-Item -Recurse -Force temp
